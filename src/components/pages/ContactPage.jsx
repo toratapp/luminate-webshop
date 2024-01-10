d4b64b1a1c7bfaa8b159083b0c8c8db21290933c
toratapp <73744488+toratapp@users.noreply.github.com>
2024-01-10 23:14:00 +0100

    Adds content to contact page

--- src/components/pages/ContactPage.jsx
+++ src/components/pages/ContactPage.jsx
@@ -8,27 +8,50 @@ function ContactPage() {
       <div className="flex justify-center">
         <form className="p-8 w-full">
           <div className="flex w-full component-preview p-4 items-center justify-center gap-2 font-sans">
-            <div className="form-control w-full max-w-xs">
+            <div className="form-control w-full max-w-xs items-center">
               <label className="label">
                 <span className="label-text">Full name</span>
               </label>
-              <Input />
+              <Input as="input" type="text" />
               <label className="error-message label">
                 <span className="label-text-alt">Error message</span>
               </label>
             </div>
           </div>
           <div className="flex w-full component-preview p-4 items-center justify-center gap-2 font-sans">
-            <Textarea />
+            <div className="form-control w-full max-w-xs items-center">
+              <label className="label">
+                <span className="label-text">Subject</span>
+              </label>
+              <Input as="input" type="text" />
+              <label className="error-message label">
+                <span className="label-text-alt">Error message</span>
+              </label>
+            </div>
           </div>
-          <label className="error-message label">
-            <span className="label-text-alt">Error message</span>
-          </label>
+          <div className="flex w-full component-preview p-4 items-center justify-center gap-2 font-sans">
+            <div className="form-control w-full max-w-xs items-center">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <Input as="input" type="email" />
+              <label className="error-message label">
+                <span className="label-text-alt">Error message</span>
+              </label>
+            </div>
+          </div>
+          <div className="flex flex-col w-full component-preview p-4 items-center justify-center gap-2 font-sans">
+            <label className="label">
+              <span className="label-text">Your message</span>
+            </label>
+            <Textarea />
+            <label className="error-message label">
+              <span className="label-text-alt">Error message</span>
+            </label>
+          </div>
         </form>
       </div>
     </section>
-  
-
   );
 }
 
